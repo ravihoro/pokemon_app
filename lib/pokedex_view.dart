94ecfc12a5a72a324e55282d8673507ed20e5341
ravihoro <ravijohnhoro5@gmail.com>
2021-04-25 11:13:39 +0530

    Added model for data

---
+++ lib/pokedex_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PokedexView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Pokedex'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
---
+++ lib/pokemon_page_response.dart
@@ -0,0 +1,42 @@
+import 'package:meta/meta.dart';
+
+class PokemonListing {
+  final int id;
+  final String name;
+
+  PokemonListing({
+    @required this.id,
+    @required this.name,
+  });
+
+  factory PokemonListing.fromJson(Map<String, dynamic> json) {
+    final name = json['name'];
+    final url = json['url'] as String;
+    List<String> list = url.split("/");
+    final id = list[list.length - 1];
+    return PokemonListing(
+      id: int.parse(id),
+      name: name,
+    );
+  }
+}
+
+class PokemonPageResponse {
+  final List<PokemonListing> pokemonListings;
+  final bool canLoadNextPage;
+
+  PokemonPageResponse({
+    @required this.pokemonListings,
+    @required this.canLoadNextPage,
+  });
+
+  factory PokemonPageResponse.fromJson(Map<String, dynamic> json) {
+    final canLoadNextPage = json['next'] != null;
+    final pokemonListings =
+        (json['result'] as List).map((e) => PokemonListing.fromJson(e));
+    return PokemonPageResponse(
+      pokemonListings: pokemonListings,
+      canLoadNextPage: canLoadNextPage,
+    );
+  }
+}
